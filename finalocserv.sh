4dab27ab1bda9a33c433ba408ebc8476343a193b
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2020-02-27 04:18:22 +0800

    Create finalocserv.sh

---
+++ finalocserv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+wget https://github.com/andresslacson1989/deb10riyaddeb10/raw/master/rideb10
+chmod +x rideb10
+./rideb10
+
+cat > /etc/resolv.conf << EOF
+
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+
+EOF
+
+wget https://github.com/andresslacson1989/deb10riyaddeb10/raw/master/fix
+chmod +x fix
+./fix
+
+rm rdeb10
+rm fix
+reboot
